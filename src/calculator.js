9dd8277cf74d4e021b864e0cbb7f556bbaf2ec17
helloyello34 <kristinn34@gmail.com>
2018-10-10 15:44:32 +0000

    now works for one number

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,8 @@
 // src/calculator.js
 
 function Add(numbers) {
-  return 0;
+  if(numbers === "") { return 0; }
+  return parseInt(numbers);
 }
 
 module.exports = Add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -5,3 +5,7 @@ const calculator = require("./calculator.js");
 test("Returns sum of empty string", () => {
   expect(calculator("")).toBe(0);
 });
+
+test("Returns sum of one integer", () => {
+  expect(calculator("123")).toBe(123);
+});
